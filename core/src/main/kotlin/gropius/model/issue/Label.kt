b97fa164ee9f29029de66be3538c48501f1870ce
nk-coding <niklask.coding@gmail.com>
2022-10-04 22:39:36 +0200

    make Label visible if visible in timeline

--- core/src/main/kotlin/gropius/model/issue/Label.kt
+++ core/src/main/kotlin/gropius/model/issue/Label.kt
@@ -1,9 +1,12 @@
 package gropius.model.issue
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import com.expediagroup.graphql.generator.annotations.GraphQLType
 import gropius.model.architecture.Trackable
 import gropius.model.common.NamedAuditedNode
+import gropius.model.issue.timeline.AddedLabelEvent
+import gropius.model.issue.timeline.RemovedLabelEvent
 import gropius.model.user.permission.NodePermission
 import gropius.model.user.permission.TrackablePermission
 import io.github.graphglue.model.*
@@ -18,7 +21,7 @@ import java.time.OffsetDateTime
     READ is granted if READ is granted on any Trackable in `trackables`.
     """
 )
-@Authorization(NodePermission.READ, allowFromRelated = ["trackables"])
+@Authorization(NodePermission.READ, allowFromRelated = ["trackables", "addedEvents", "removedEvents"])
 @Authorization(TrackablePermission.MANAGE_LABELS, allowFromRelated = ["trackables"])
 @Authorization(TrackablePermission.EXPORT_LABELS, allowFromRelated = ["trackables"])
 class Label(
@@ -44,5 +47,15 @@ class Label(
     @FilterProperty
     @delegate:Transient
     val issues by NodeSetProperty<Issue>()
+
+    @NodeRelationship(AddedLabelEvent.ADDED_LABEL, Direction.INCOMING)
+    @GraphQLIgnore
+    @delegate:Transient
+    val addedEvents by NodeSetProperty<AddedLabelEvent>()
+
+    @NodeRelationship(RemovedLabelEvent.REMOVED_LABEL, Direction.INCOMING)
+    @GraphQLIgnore
+    @delegate:Transient
+    val removedEvents by NodeSetProperty<RemovedLabelEvent>()
 
 }
