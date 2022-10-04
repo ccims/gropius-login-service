a4b178aa81c2cf3ff93bd3f4cee46a9ae56b05eb
nk-coding <niklask.coding@gmail.com>
2022-10-04 14:24:42 +0200

    fix timeline items and repositories

--- core/src/main/kotlin/gropius/model/issue/timeline/ClosedEvent.kt
+++
@@ -1,9 +0,0 @@
-package gropius.model.issue.timeline
-
-import com.expediagroup.graphql.generator.annotations.GraphQLDescription
-import io.github.graphglue.model.DomainNode
-import java.time.OffsetDateTime
-
-@DomainNode
-@GraphQLDescription("Event representing that an Issue was closed.")
-class ClosedEvent(createdAt: OffsetDateTime, lastModifiedAt: OffsetDateTime) : TimelineItem(createdAt, lastModifiedAt)
---
+++ core/src/main/kotlin/gropius/model/issue/timeline/StateChangedEvent.kt
@@ -0,0 +1,35 @@
+package gropius.model.issue.timeline
+
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import gropius.model.template.IssueState
+import io.github.graphglue.model.Direction
+import io.github.graphglue.model.DomainNode
+import io.github.graphglue.model.FilterProperty
+import io.github.graphglue.model.NodeRelationship
+import org.springframework.data.annotation.Transient
+import java.time.OffsetDateTime
+
+@DomainNode
+@GraphQLDescription("Event representing that the state of an Issue changed.")
+class StateChangedEvent(
+    createdAt: OffsetDateTime,
+    lastModifiedAt: OffsetDateTime,
+) : TimelineItem(createdAt, lastModifiedAt) {
+
+    companion object {
+        const val OLD_STATE = "OLD_STATE"
+        const val NEW_STATE = "NEW_STATE"
+    }
+
+    @NodeRelationship(OLD_STATE, Direction.OUTGOING)
+    @GraphQLDescription("The old state.")
+    @FilterProperty
+    @delegate:Transient
+    val oldState by NodeProperty<IssueState>()
+
+    @NodeRelationship(NEW_STATE, Direction.OUTGOING)
+    @GraphQLDescription("The new state.")
+    @FilterProperty
+    @delegate:Transient
+    val newState by NodeProperty<IssueState>()
+}
