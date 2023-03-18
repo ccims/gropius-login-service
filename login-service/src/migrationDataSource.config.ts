47df3640890eb8154c4ce61b2b2083cbe89bea82
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-03-18 03:20:00 +0100

    Fixed migration data source

--- login-service/src/migrationDataSource.config.ts
+++ login-service/src/migrationDataSource.config.ts
@@ -1,21 +1,7 @@
-import { DataSource, EntityManager, DataSourceOptions } from "typeorm";
-import { config } from "dotenv";
+import { DataSource } from "typeorm";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import { TokenScope } from "./backend-services/token.service";
-import { OpenApiTag } from "./openapi-tag";
 import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserLoginData } from "./model/postgres/UserLoginData.entity";
-
-/* if (process.env.NODE_ENV === "production") {
-    config({ path: ".env.prod.local", override: false });
-    config({ path: ".env.prod", override: false });
-} else {
-    config({ path: ".env.dev.local", override: false });
-    config({ path: ".env.dev", override: false });
-} */
 
 async function getDataSource(): Promise<DataSource> {
     process.env.NODE_ENV = "production";
