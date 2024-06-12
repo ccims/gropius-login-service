c69927b3d2bc1352b095a62ff9e75bc12adbf735
Christian Kurz <chrikuvellberg@gmail.com>
2024-06-12 03:03:30 +0200

    port issue template/type

--- jira/src/main/kotlin/gropius/sync/jira/config/IMSConfig.kt
+++ jira/src/main/kotlin/gropius/sync/jira/config/IMSConfig.kt
@@ -14,7 +14,12 @@ import java.net.URI
  * @param imsTemplate the template of the current IMS
  */
 data class IMSConfig(
-    val botUser: String, val readUser: String?, val rootUrl: URI, val imsTemplate: IMSTemplate
+    val botUser: String,
+    val readUser: String?,
+    val rootUrl: URI,
+    val imsTemplate: IMSTemplate,
+    val defaultType: String?,
+    val defaultTemplate: String?
 ) {
     /**
      * @param ims the Gropius ims to use as input
@@ -27,7 +32,9 @@ data class IMSConfig(
         botUser = helper.parseString(ims.templatedFields["bot-user"]) ?: "jira-bot",
         readUser = helper.parseString(ims.templatedFields["read-user"]),
         rootUrl = URI(helper.parseString(ims.templatedFields["root-url"])!!),
-        imsTemplate = imsTemplate
+        imsTemplate = imsTemplate,
+        defaultType = helper.parseString(ims.templatedFields["default-type"]),
+        defaultTemplate = helper.parseString(ims.templatedFields["default-template"])
     )
 
     companion object {
@@ -50,6 +57,18 @@ data class IMSConfig(
             "type" to "string"
             "metadata" to obj {
                 "format" to "uri"
+            }
+        }.toString(), "default-type" to obj {
+            "nullable" to true
+            "type" to "string"
+            "metadata" to obj {
+                "gropius-node" to "IssueType"
+            }
+        }.toString(), "default-template" to obj {
+            "nullable" to true
+            "type" to "string"
+            "metadata" to obj {
+                "gropius-node" to "IssueTemplate"
             }
         }.toString()) + IMSConfigManager.COMMON_TEMPLATE_FIELDS
     }
