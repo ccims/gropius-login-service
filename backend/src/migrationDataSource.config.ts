7745576cf8289f21317c1e107f8a23b7975e2c29
Miles Stötzner <miles@stoetzner.de>
2025-10-12 21:21:10 +0200

    mv datasource back

--- backend/src/migrationDataSource.config.ts
+++ backend/src/migrationDataSource.config.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm";
 async function getDataSource(): Promise<DataSource> {
     console.log(
         "NOTICE: Make sure to run migrations with NODE_ENV set to the value for which " +
-            "you want to initiaize your database and have configured your database settings.\n" +
+            "you want to initialize your database and have configured your database settings.\n" +
             "E.g. if you have your DB settings in .env.dev, set NODE_ENV=testing",
     );
 
     const { NestFactory } = await import("@nestjs/core");
-    const { AppModule } = await import("../app.module");
+    const { AppModule } = await import("./app.module");
     const { ConfigModule } = await import("@nestjs/config");
 
     const app = await NestFactory.create(AppModule, {
